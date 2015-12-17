61eeaa3978a08f44344d37f6e7756d37ca186a3c
Hao Duan <haoduan@outlook.com>
2015-12-17 18:30:48 +1300

    update @ 6:30PM 17/12/2015

--- activity/activity.php
+++ activity/activity.php
@@ -22,11 +22,17 @@ if ( !function_exists( 'add_action' ) ) {
 define( 'ACTIVITY__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'ACTIVITY__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-register_activation_hook( __FILE__, array( 'Activity', 'plugin_activation' ) );
-register_deactivation_hook( __FILE__, array( 'Activity', 'plugin_deactivation' ) );
+register_activation_hook( __FILE__, array( 'Activity', 'activity_activation' ) );
+register_deactivation_hook( __FILE__, array( 'Activity', 'activity_deactivation' ) );
 
 require_once( ACTIVITY__PLUGIN_DIR . 'class.activity.php' );
 
-add_action( 'init', array( 'Activity', 'init' ) );
+add_action( 'init', array( 'Activity', 'activity_init' ) );
+
+// Code blow enables debug message output during plugin activation.
+add_action( 'activated_plugin', 'my_save_error' );
+function my_save_error() {
+	file_put_contents( ACTIVITY__PLUGIN_DIR . 'error_activation.txt', ob_get_contents() );
+}
 
 ?>
--- activity/class.activity.php
+++ activity/class.activity.php
@@ -1,43 +1,68 @@
 <?php
 
 class Activity {
-	private static $initialed = false;
+	private static $activity_initialed = false;
 	
-	public static function init() {
-		if ( !self::$initialed ) {
-			self::init_hooks();
+	public static function activity_init() {
+		if ( !self::$activity_initialed ) {
+			self::activity_init_hooks();
 		}
 	}
 	
-	public static function plugin_activation() {
+	public static function activity_activation() {
 		$activity_init_terms = get_terms( 'category', 'orderby=id&hide_empty=0' );
 		$activity_init_current_option = get_option( 'activity_category', 'false' );
-		
-		if ( $activity_init_current_option != 'false' ) {
-			foreach ( $activity_init_terms as $activity_init_term ) {
-				if ( $activity_init_current_option == $activity_init_term->term_id ) {
-					return;
-				}
-			}
-		}
+		$activity_option_matches_term = false;
 		
 		if ( ! empty( $activity_init_terms ) && ! is_wp_error( $activity_init_terms ) ) {
-			update_option( 'activity_category', $activity_init_terms[0]->term_id );
+			if ( $activity_init_current_option != 'false' ) {
+				foreach ( $activity_init_terms as $activity_init_term ) {
+					if ( $activity_init_current_option == $activity_init_term->term_id ) {
+						$activity_option_matches_term = true;
+						break;
+					}
+				}
+				if ( ! $activity_option_matches_term )
+				{
+					update_option( 'activity_category', $activity_init_terms[0]->term_id );
+				}
+			} else {
+				update_option( 'activity_category', $activity_init_terms[0]->term_id );
+			}
 		} else {
 			update_option( 'activity_category', 0 );
 		}
 		
-		self::init_database();
+		if ( ! self::activity_is_table_created() ) {
+			self::activity_init_database();
+		}
 	}
 	
-	public static function plugin_deactivation() {
+	public static function activity_deactivation() {
 		
+	}
+	
+	public static function activity_uninstall() {
+		
+	}
+	
+	private static function activity_is_table_created() {
+		global $wpdb;
+		$activity_table_dbname = DB_NAME;
+		$activity_table_name = $wpdb->prefix . "activity_signup";
+		$activity_table_sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$activity_table_dbname' AND table_name = '$activity_table_name'";
+		$activity_table_count = $wpdb->get_var( $activity_table_sql );
+		
+		if ( $activity_table_count > 0 ) {
+			return true;
+		}
+		return false;
 	}
 	
 	/**
 	 * Initialize plugin database
 	 */
-	private static function init_database() {
+	private static function activity_init_database() {
 		global $wpdb;
 		$activity_table_name = $wpdb->prefix . "activity_signup";
 		$activity_charset_collate = $wpdb->get_charset_collate();
@@ -61,7 +86,7 @@ class Activity {
 	/**
 	 * Initialize wordpress hooks
 	 */
-	private static function init_hooks() {
+	private static function activity_init_hooks() {
 		
 	}
 }
