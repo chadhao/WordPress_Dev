e14c3d0515aed65b3f4c5e56679097f95a7e7ea1
Hao Duan <haoduan@outlook.com>
2016-01-12 16:18:02 +1300

    update @ 4:17PM 12/01/2016

--- activity/views/activity_admin_list.php
+++ activity/views/activity_admin_list.php
@@ -1,3 +1,31 @@
+<?php
+
+$activity_args = array(
+	'category' => get_option( 'activity_category' ),
+	'orderby' => 'date',
+	'order' => 'DESC',
+);
+$all_activity = get_posts( $activity_args );
+?>
 <div class="wrap">
 	<h1>活动列表 <a href="#" class="page-title-action">添加活动</a></h1>
+	<table>
+		
+		<?php
+		if ( ! empty( $all_activity ) ) {
+			foreach ( $all_activity as $activity ) {
+		?>
+		<tr>
+			<td>ID</td>
+			<td>Title</td>
+			<td>Last modified</td>
+		</tr>
+		<?php
+				echo '<tr><td>' . $activity -> ID .'</td><td>' . $activity -> post_title . '</td><td>' . $activity -> post_modified . '</td></tr>';
+			}
+		} else {
+			echo '<h2>No activities!</h2>';
+		}
+		?>
+	</table>
 </div>
