56d62816a3d612aefaa9789b81d155aa624f0712
Hao Duan <haoduan@outlook.com>
2016-02-02 01:05:20 +1300

    update @ 1:05PM 02/02/2016

--- activity/views/activity_admin_post.php
+++ activity/views/activity_admin_post.php
@@ -3,34 +3,32 @@ include 'style.php';
 ?>
 
 <div class="wrap">
-	<h1><?php echo $_GET['post_action']=='add'?'添加活动':'编辑活动'; ?></h1>
-	
-	
-	<h1></h1>
-	
-	<form class="am-form am-form-horizontal" action="">
+	<form class="am-form" action="">
 		<fieldset>
+			<legend><?php echo $_GET['post_action']=='add'?'添加活动':'编辑活动'; ?></legend>
+			
 			<input type="hidden" id="post_id" value="<?php echo $_GET['post_action']=='add'?'new':$_GET['post_id']; ?>">
 			
 			<div class="am-form-group">
-				<label for="title" class="am-u-sm-2 am-form-label">活动标题</label>
-				<div class="am-u-sm-10">
-					<input type="text" id="title" placeholder="请输入活动标题">
-				</div>
+				<label for="title">活动标题</label>
+				<input type="text" id="title" placeholder="请输入活动标题">
 			</div>
 
 			<div class="am-form-group">
-				<label for="location" class="am-u-sm-2 am-form-label">活动地点</label>
-				<div class="am-u-sm-10">
-					<input type="text" id="location" placeholder="请输入活动地点">
-				</div>
+				<label for="location">活动地点</label>
+				<input type="text" id="location" placeholder="请输入活动地点">
 			</div>
 
 			<div class="am-form-group">
-				<label for="activity_time" class="am-u-sm-2 am-form-label">活动时间</label>
-				<div class="am-u-sm-10">
-					<input type="datetime-local" id="activity_time" placeholder="请选择活动时间">
-				</div>
+				<label for="activity_date">活动日期</label>
+				<input type="text" id="activity_date" placeholder="请选择活动日期" class="am-form-field" data-am-datepicker readonly>
+			</div>
+			
+			<div class="am-form-group">
+				<label for="activity_time_hour">活动时间</label>
+				<select id="activity_time_hour">
+						<?php for ( $i=0; $i<24; $i++) { echo '<option value="' . $i . '">' . $i . '</option>'; } ?>
+				</select>
 			</div>
 		</fieldset>
 	</form>
