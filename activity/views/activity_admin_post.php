7416e685cfd7d9ab17e587820fa0e1de1977c229
Hao Duan <haoduan@outlook.com>
2016-02-02 10:22:28 +1300

    update @ 10：22AM 02/02/2016

--- activity/views/activity_admin_post.php
+++ activity/views/activity_admin_post.php
@@ -3,7 +3,7 @@ include 'style.php';
 ?>
 
 <div class="wrap">
-	<form class="am-form" action="">
+	<form class="am-form am-u-sm-6" action="">
 		<fieldset>
 			<legend><?php echo $_GET['post_action']=='add'?'添加活动':'编辑活动'; ?></legend>
 			
@@ -19,15 +19,15 @@ include 'style.php';
 				<input type="text" id="location" placeholder="请输入活动地点">
 			</div>
 
-			<div class="am-form-group">
+			<div class="am-form-group am-u-sm-8">
 				<label for="activity_date">活动日期</label>
 				<input type="text" id="activity_date" placeholder="请选择活动日期" class="am-form-field" data-am-datepicker readonly>
 			</div>
 			
-			<div class="am-form-group">
-				<label for="activity_time_hour">活动时间</label>
-				<select id="activity_time_hour">
-						<?php for ( $i=0; $i<24; $i++) { echo '<option value="' . $i . '">' . $i . '</option>'; } ?>
+			<div class="am-form-group  am-u-sm-4">
+				<label for="activity_time">活动时间</label>
+				<select id="activity_time">
+						<?php for ( $i=0; $i<144; $i++) { echo '<option value="' . sprintf("%02d", floor($i/6)) . ':' . sprintf("%02d", floor($i%6*10)) . '">' . sprintf("%02d", floor($i/6)) . ':' . sprintf("%02d", floor($i%6*10)) . '</option>'; } ?>
 				</select>
 			</div>
 		</fieldset>
