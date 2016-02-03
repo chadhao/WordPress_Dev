386c45a58cbec6a5de5d651b51c51ad6908a2a78
Hao Duan <haoduan@outlook.com>
2016-02-03 19:06:18 +1300

    update @ 7:06PM 03/02/2016

--- activity/views/activity_admin_post.php
+++ activity/views/activity_admin_post.php
@@ -56,7 +56,7 @@ include 'style.php';
 			
 			<div class="am-form-group am-g">
 				<label for="poster_upload">活动海报</label>
-				<form class="am-form am-g" id="poster_upload" action="processupload.php" method="post" enctype="multipart/form-data">
+				<form class="am-form am-g" id="poster_upload" action="" method="post" enctype="multipart/form-data">
 					<input name="poster_image" id="poster_image" type="file">
 					<button type="submit" id="poster_submit">上传</button>
 				</form>
--- activity/views/style.php
+++ activity/views/style.php
@@ -2,6 +2,4 @@
 echo '<style type="text/css"> @import url("https://cdnjs.cloudflare.com/ajax/libs/amazeui/2.5.2/css/amazeui.min.css"); </style>';
 
 echo '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/amazeui/2.5.2/js/amazeui.min.js"></script>';
-echo '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>';
-echo '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>';
 ?>
